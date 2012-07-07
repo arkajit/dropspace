fdb934ddbc39bd6a39bf12f6c35f6486f5d583bc
Arkajit Dey <arkajit.dey@gmail.com>
2012-07-07 13:50:28 -0700

    Added basic styles. Switched back from gunicorn to flask's default server for easier debugging during development.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -11,4 +11,4 @@ def hello(name=None):
 
 if __name__ == '__main__':
   port = int(os.environ.get('PORT', 5000))
-  app.run(host = '0.0.0.0', port=port)
+  app.run(host = '0.0.0.0', port=port, debug=True)
